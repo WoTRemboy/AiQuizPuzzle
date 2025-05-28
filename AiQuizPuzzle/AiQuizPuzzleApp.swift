936b53b19cb6dfe0ebe5947d1cae84c4b3341d6f
Roman Tverdokhleb <tverpokhdeb@icloud.com>
2025-05-28 18:35:55 +0300

    Initial Commit

---
+++ AiQuizPuzzle/AiQuizPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiQuizPuzzleApp.swift
+//  AiQuizPuzzle
+//
+//  Created by Roman Tverdokhleb on 28/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct AiQuizPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
