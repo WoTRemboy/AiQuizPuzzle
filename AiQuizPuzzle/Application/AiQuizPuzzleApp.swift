3e1a18d949eb1b90771fd2fafc01af2bc4d90f60
Roman Tverdokhleb <tverpokhdeb@icloud.com>
2025-05-29 15:26:37 +0300

    added firebase dependence & configuration

--- AiQuizPuzzle/Application/AiQuizPuzzleApp.swift
+++ AiQuizPuzzle/Application/AiQuizPuzzleApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct AiQuizPuzzleApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             SplashScreenView()
