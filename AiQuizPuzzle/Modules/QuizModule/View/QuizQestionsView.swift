44ecd2e8a872ff97ddf1ff85c6b9aa723acce1fb
Roman Tverdokhleb <tverpokhdeb@icloud.com>
2025-05-30 22:56:40 +0300

    added answer selecting method

--- AiQuizPuzzle/Modules/QuizModule/View/QuizQestionsView.swift
+++ AiQuizPuzzle/Modules/QuizModule/View/QuizQestionsView.swift
@@ -9,11 +9,12 @@ import SwiftUI
 
 struct QuizQestionsView: View {
     private var info: QuizInfo
-    private var questions: [QuizQuestion]
+    @State private var questions: [QuizQuestion]
     
     @Environment(\.dismiss) private var dismiss
     @State private var progress: CGFloat = 0
     @State private var currentIndex: Int = 0
+    @State private var score: CGFloat = 0
     
     init(info: QuizInfo, questions: [QuizQuestion]) {
         self.info = info
@@ -66,9 +67,14 @@ struct QuizQestionsView: View {
             .padding(.horizontal, -15)
             .padding(.vertical, 15)
             
-            CustomButton(title: "Next Question") {
-                withAnimation {
-                    currentIndex += 1
+            CustomButton(title: currentIndex == (questions.count - 1) ? "Finish" : "Next Question") {
+                if currentIndex == (questions.count - 1) {
+                    
+                } else {
+                    withAnimation {
+                        currentIndex += 1
+                        progress = CGFloat(currentIndex) / CGFloat(questions.count)
+                    }
                 }
             }
         }
@@ -82,10 +88,71 @@ struct QuizQestionsView: View {
     }
     
     @ViewBuilder
-    private func questionView(_ quiestion: QuizQuestion) -> some View {
-        RoundedRectangle(cornerRadius: 20, style: .continuous)
-            .fill(.white)
+    private func questionView(_ question: QuizQuestion) -> some View {
+        VStack(alignment: .leading, spacing: 15) {
+            Text("Question \(currentIndex + 1)/\(questions.count)")
+                .font(.callout)
+                .foregroundStyle(Color.gray)
+                .hAlign(.leading)
+            
+            Text(question.question)
+                .font(.title3)
+                .fontWeight(.semibold)
+                .foregroundStyle(.black)
+            
+            VStack(spacing: 12) {
+                ForEach(question.options, id: \.self) { option in
+                    ZStack {
+                        optionView(option, tint: .gray)
+                            .opacity(question.answer == option
+                                     && question.tappedAnswer != "" ? 0 : 1)
+                        optionView(option, tint: .green)
+                            .opacity(question.answer == option
+                                     && question.tappedAnswer != "" ? 1 : 0)
+                        optionView(option, tint: .red)
+                            .opacity(question.tappedAnswer == option
+                                     && question.tappedAnswer != question.answer ? 1 : 0)
+                    }
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        guard questions[currentIndex].tappedAnswer == "" else { return }
+                        withAnimation(.easeInOut) {
+                            questions[currentIndex].tappedAnswer = option
+                            
+                            if question.answer == option {
+                                score += 1
+                            }
+                        }
+                    }
+                }
+            }
+            .padding(.vertical, 10)
+        }
+        .padding(15)
+        .hAlign(.center)
+        .background {
+            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                .fill(.white)
+        }
+        .padding(.horizontal, 15)
+    }
+    
+    @ViewBuilder
+    func optionView(_ option: String, tint: Color) -> some View {
+        Text(option)
+            .foregroundStyle(tint)
             .padding(.horizontal, 15)
+            .padding(.vertical, 20)
+            .hAlign(.leading)
+            .background {
+                RoundedRectangle(cornerRadius: 12, style: .continuous)
+                    .fill(tint.opacity(0.15))
+                    .background {
+                        RoundedRectangle(cornerRadius: 12, style: .continuous)
+                            .stroke(tint.opacity(tint == .gray ? 0.15 : 1),
+                                    lineWidth: 2)
+                    }
+            }
     }
 }
 
